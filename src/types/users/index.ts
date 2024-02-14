219168005b6c3c9c88bf8217e6fc88648b583c81
Thang Ngo <ngosythang123456@gmail.com>
2024-02-14 15:03:02 +0700

    add file index

---
+++ src/types/users/index.ts
@@ -0,0 +1,4 @@
+export type { default as GetAllUserResponse } from "./GetAllUserResponse";
+export type { default as PostUserRequest } from "./PostUserRequest";
+export type { default as PutBannedUserRequest } from "./PutBannedUserRequest";
+export type { default as PutRoleUserRequest } from "./PutRoleUserRequest";
