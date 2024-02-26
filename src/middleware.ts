8dba596f2013660c326c3f981e79c4a786efcc1b
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-26 11:47:53 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export const config = {
     matcher: ["/", "/auth/:path*", "/404", "/user/:path*", "/category/:path*"],
 }
 
-export const publicRoutes: String[] = ["/404"]
+export const publicRoutes: string[] = ["/404"]
 export const authRoutes = ["/auth/login", "/auth/register"]
 export const protectedRoutes = [
     {
@@ -35,20 +35,42 @@ export const middleware = async (req: NextRequest) => {
         (await verifyAuth(token).catch((error) => {
             console.error(error)
         }))
-
+    if (req.nextUrl.pathname === "/auth/profile") {
+        if (!userData) {
+            return null
+        }
+        if (verifiedToken) {
+            return NextResponse.json({
+                user: decodedUserData,
+                token,
+            })
+        }
+    }
     if (req.nextUrl.pathname === "/auth/logout") {
-        const response = NextResponse.redirect(new URL("/auth/login", req.url))
+        const redirect = req.nextUrl.searchParams.get("r")
+
+        const response = NextResponse.redirect(
+            new URL(
+                redirect ? `/auth/login?r=${redirect}` : "/auth/login",
+                req.url
+            )
+        )
         response.cookies.set("token", "", { expires: new Date(0) })
         response.cookies.set("user", "", { expires: new Date(0) })
         return response
     }
 
-    const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname)
-    const isAuthRoute = authRoutes.includes(req.nextUrl.pathname)
     const protectedRoute = protectedRoutes.find((route) =>
         req.nextUrl.pathname.startsWith(route.path)
     )
     const isProtectedRoute = !!protectedRoute
+
+    const isAuthRoute = authRoutes.includes(req.nextUrl.pathname)
+
+    const isPublicRoute =
+        publicRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) &&
+        !isAuthRoute &&
+        !isProtectedRoute
 
     if (isPublicRoute) {
         return NextResponse.next()
@@ -61,16 +83,18 @@ export const middleware = async (req: NextRequest) => {
     }
 
     if (!token) {
-        return NextResponse.redirect(new URL("/auth/login", req.url))
+        return NextResponse.redirect(
+            new URL(` /auth/login?r=${req.nextUrl.pathname}`, req.url)
+        )
     }
 
     if (isProtectedRoute) {
-        if (protectedRoute.roles.includes(decodedUserData?.role)) {
+        if (protectedRoute.roles.includes(decodedUserData.role)) {
             return NextResponse.next()
         } else {
-            const url = req.nextUrl
-            url.pathname = `/404`
-            return NextResponse.rewrite(url)
+            return NextResponse.redirect(
+                new URL(`/auth/logout?r=${req.nextUrl.pathname}`, req.url)
+            )
         }
     }
 
@@ -78,5 +102,7 @@ export const middleware = async (req: NextRequest) => {
         return NextResponse.next()
     }
 
-    return NextResponse.redirect(new URL("/auth/login", req.url))
+    return NextResponse.redirect(
+        new URL(`/auth/login?r=${req.nextUrl.pathname}`, req.url)
+    )
 }
