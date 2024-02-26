bc793c6b1a486d45ccce205d9cd0167eeb350202
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-26 11:53:16 +0700

    fix: api endpoint

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const instance = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: process.env.API_URL || "http://localhost:6868/v1",
     withCredentials: true,
 })
 
