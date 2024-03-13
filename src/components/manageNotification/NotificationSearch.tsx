5614062b5dd2faa10e63df242d1ca9fa13506ea0
Thang Ngo <ngosythang123456@gmail.com>
2024-03-13 09:01:32 +0700

    feat: search notification

--- src/components/manageNotification/NotificationSearch.tsx
+++ src/components/manageNotification/NotificationSearch.tsx
@@ -1,10 +1,44 @@
-import { ActionIcon, Group, TextInput, rem } from "@mantine/core"
+import { ActionIcon, Group, TextInput, rem } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 
-const NotificationSearch=()=>{
-    return(
-        <Group gap="3px" wrap="nowrap">
+interface Query {
+  [key: string]: string | string[] | undefined;
+  keyword?: string | string[];
+  order?: string;
+  sort?: string;
+  target?: string | string[];
+}
+const NotificationSearch = () => {
+  const router = useRouter();
+  const { keyword, target } = router.query;
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  useEffect(() => {
+    if (router.isReady) {
+      setSearchKeyword((keyword as string) || "");
+    }
+  }, [router.isReady]);
+  const handdleSearch = () => {
+    if (!searchKeyword || searchKeyword.length === 0) {
+      router.push({
+        query: {},
+      });
+      return;
+    }
+    const queryy: Query = {};
+    if (target) {
+      queryy.target = target;
+    }
+    queryy.keyword = searchKeyword;
+    router.push({
+      query: queryy,
+    });
+  };
+  return (
+    <Group gap="3px" wrap="nowrap">
       <TextInput
         placeholder="Search"
         size="xs"
@@ -18,13 +52,13 @@ const NotificationSearch=()=>{
         rightSectionWidth={70}
         styles={{ section: { pointerEvents: "none" } }}
         mb="md"
-        // value={searchKeyword}
-        // onChange={(e) => setSearchKeyword(e.currentTarget.value)}
+        value={searchKeyword}
+        onChange={(e) => setSearchKeyword(e.currentTarget.value)}
       />
-      <ActionIcon mb={"md"} >
+      <ActionIcon mb={"md"} onClick={handdleSearch}>
         <CiSearch />
       </ActionIcon>
     </Group>
-    )
-}
-export default NotificationSearch
+  );
+};
+export default NotificationSearch;
--- src/components/manageNotification/index.ts
+++ src/components/manageNotification/index.ts
@@ -3,3 +3,4 @@ export { default as CreateNotificationBtn } from "./CreateNotificationBtn";
 export { default as NotificationTable } from "./NotificationTable";
 export { default as NotificationDetailModal } from "./NotificationDetailModal";
 export { default as UpdateNotificationModal } from "./UpdateNotificationModal";
+export { default as NotificationPagination } from "./NotificationPagination";
